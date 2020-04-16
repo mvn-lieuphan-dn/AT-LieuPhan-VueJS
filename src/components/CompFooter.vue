f1c33519fc186d471eb93e7df467fa51336bcdc2
at-lieuphan <lieu.phan@asiantech.vn>
2020-04-16 23:04:54 +0700

    first commit

---
+++ src/components/CompFooter.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ title }}</p>
+</template>
+
+<script>
+export default {
+  name: 'comp-footer',
+  data() {
+    return {
+      title: 'Footer page'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/CompHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ title }} </p>
+</template>
+
+<script>
+export default {
+  name: 'comp-header',
+  data() {
+    return {
+      title: 'Header page'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
